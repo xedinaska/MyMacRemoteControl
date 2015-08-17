9782b3a9eb11e843faba18c126dfe392c5bb0f79
Polevoy Alexander <xedinaska@gmail.com>
2015-08-17 22:14:35 +0300

    feat(driver_vlc): added base controls (arrows/play&pause/etc)

--- Mac/Controller/VLCController.py
+++ Mac/Controller/VLCController.py
@@ -8,7 +8,17 @@ class VLCController:
     def handle(self, strCode):
         driver = Mac.Driver.OsXSystemDriver.OsXSystemDriver()
 
-        if strCode == 0:
+        if strCode == 'OK':
+            driver.pressButton('p', 'using {command down}')
+        elif strCode == 0:
             driver.pressButton('q', 'using {command down}')
         elif strCode == '#':
             driver.pressButton('f', 'using {command down, control down}')
+        elif strCode == 'ARR_UP':
+            driver.pressButton(driver.ARROW_UP_CODE, 'using {command down}')
+        elif strCode == 'ARR_DOWN':
+            driver.pressButton(driver.ARROW_DOWN_CODE, 'using {command down}')
+        elif strCode == 'ARR_LEFT':
+            driver.pressButton(driver.ARROW_LEFT_CODE, 'using {command down}')
+        elif strCode == 'ARR_RIGHT':
+            driver.pressButton(driver.ARROW_RIGHT_CODE, 'using {command down}')
